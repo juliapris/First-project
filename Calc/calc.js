022152dcdef34359bab11ebff46154b5918b32a7
Julia Sidorova email = pris_julia@mail.ru <pris_julia@mail.ru>
2021-11-02 20:15:01 +0300

    Add a new calculator

--- Calc/calc.js
+++ Calc/calc.js
@@ -1,25 +1,27 @@
-function calc (a,b,operator){
+"use strict";
+function calc (a, b, operator) {
     let result;
-    if (a === "") || b === ""){
-    result = "Error";
-} else if (isNaN(a)) || isNaN(b)){
-    result = "Error";
-} else if (operator === "sum"){
-    result = a+b;
-} else if (operator === "sub"){
-    result = a-b;
-} else if (operator === "divide"){
-    if b === 0{
-        result ="Error. Division by zero is forbidden";
-    }else{
-    result = a/b;}
-      
-} else if (operator === "multip"){
+    const isNotValid = ( !a || !b || (typeof a != "number") || (typeof b != "number") );
+        if (isNotValid) {
+    result = "Ошибка";
+    } else if (!operator) {
+    result = "Неизвестный оператор";
+    } else if (operator === "sum") {
+    result = a + b;
+    } else if (operator === "sub") {
+    result = a - b;
+    } else if (operator === "divide") {
+    if (b === 0) {
+    result ="Ошибка. Делить на ноль нельзя.";
+    } else {
+    result = a/b;
+    }
+    } else if (operator === "multip"){
     result = a*b;
+    }
+
+    return result;
+
+
 }
-
-return result;
-
-
-}
-console.log (calc(a=5, b=0, operator="divide")); 
+console.log(calc(a:5,b:0,operator:"divide")); 
